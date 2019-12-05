7d4f1e8e7e8318deaea9bccac3b4c8724315c59e
Captaincrogs <2003829@talnet.nl>
2019-12-05 11:16:35 +0100

    Uitwerking van de exercise

---
+++ form.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<head>
+  <title> </title>
+  <meta charset="UTF-8">
+</head>
+
+<body>
+  <?php
+  echo "Hello world";
+
+  ?>
+
+<div>
+
+  <br> <br><input type="text" name="Wat is je naam" value=" true">
+
+</div>
+
+</input>
+
+
+</body>
+
+
+<footer>
+
+</footer>
+
+
+
+
+</html>
